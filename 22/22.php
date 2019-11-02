e36b2d7eda843266cea0ad31395fd7fcd6284fa2
trungcap <ctxtrung@gmail.com>
2019-11-02 13:57:00 +0700

    f

---
+++ 22/22.php
@@ -0,0 +1,42 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+$less->compileFile('less/22.less', 'css/22.css');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>22</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/22.css" rel="stylesheet" type="text/css" />
+        <link href="css/22.css" rel="stylesheet" type="text/css" />
+
+        <script src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/custom-js.js"></script>
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/22.less', 'css/22.css');
+        ?>
+    </head>
+    <body>
+        <?php include './22-content.php'; ?>
+    </body>
+</html>
