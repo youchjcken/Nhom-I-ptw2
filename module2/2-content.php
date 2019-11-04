dcce396b7e0912a820fbac63ef904899a9564aa3
youchjckken <hieuphan2207@gmail.com>
2019-11-04 16:35:53 +0700

    commit

--- module2/2-content.php
+++ module2/2-content.php
@@ -9,45 +9,44 @@ $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-2">
     <div class="container">
-        <div class="row">
-            <div class="col-md-4  col-ms-6 rrr">
-                <div class="baner">
-                    <h2>Mobiles &amp;
-                        <br>
-                        Tablets
-                    </h2>
-                    <div class="shopnow"><span class="mkd-btn-text"><a href="#">Shop Now</a></span></div>
-                    <img src="./images/backgroud1.jpg" alt="background1">
+<div class="row">
+    <div class="col-md-4 col-sm-6">
+    <div class="baner">
+            <h2>Mobiles &amp;
+                <br>
+                Tablets
+            </h2>
+            <div class="shopnow"><span class="mkd-btn-text"><a href="#">Shop Now</a></span></div>
+            <img src="./images/backgroud1.jpg" alt="background1">
 
-                </div>
-            </div>
-            <div class="col-md-4 col-ms-6 rrr">
-                <div class="baner">
-                    <h2>Gadgets &amp;
-                        <br>
-                        Accessories
-                    </h2>
-                    <div class="shopnow"><span class="mkd-btn-text"><a href="#">Shop Now</a></span></div>
-                    <img src="./images/backgroud2.jpg" alt="background1">
-
-                </div>
-            </div>
-            <div class="col-md-4 col-ms-6 rrr">
-                <div class="baner">
-                    <h2>Computer &amp;
-                        <br>
-                        Laptops
-                    </h2>
-                    <div class="shopnow"><span class="mkd-btn-text"><a href="#">Shop Now</a></span></div>
-                    <img src="./images/backgroud3.jpg" alt="background1">
-
-                </div>
-            </div>
         </div>
+    </div>
+    <div class="col-md-4 col-sm-6">
+    <div class="baner">
+            <h2>Gadgets &amp;
+                <br>
+                Accessories
+            </h2>
+            <div class="shopnow"><span class="mkd-btn-text"><a href="#">Shop Now</a></span></div>
+            <img src="./images/backgroud2.jpg" alt="background1">
 
+        </div>
+    </div>
+    <div class="col-md-4 col-sm-6">
+    <div class="baner">
+            <h2>Computer &amp;
+                <br>
+                Laptops
+            </h2>
+            <div class="shopnow"><span class="mkd-btn-text"><a href="#">Shop Now</a></span></div>
+            <img src="./images/backgroud3.jpg" alt="background1">
 
-
-
+        </div>
+    </div>
+</div>
+       
+    
+    
     </div>
 </div>
 </div>
